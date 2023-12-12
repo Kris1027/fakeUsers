7530268b60502312cffc9d78cf53b6d892687d84
Kris1027 <obarzanek.work@gmail.com>
2023-12-12 19:17:46 +0100

    fetching users

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,53 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import User from './components/User';
+
 function App() {
-  return <div>fakeUsers</div>;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    function fakeUsers() {
+      fetch('https://reqres.in/api/users')
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return res.json();
+        })
+        .then((res) => {
+          setData(res.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    }
+
+    fakeUsers();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+      {data.map((user) => (
+        <User
+          key={user.id}
+          first_name={user.first_name}
+          last_name={user.last_name}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
